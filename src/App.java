e98b689e8733960c228c5e0aead0143928fee100
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2024-03-20 18:47:37 +0100

    Gjort klar del a och börjat del b

--- src/App.java
+++ src/App.java
@@ -5,6 +5,11 @@ public class App {
         Scanner scan = new Scanner(System.in);
         System.out.println("Ange vad 10 ska delas med");
         int var1 = 10;
-        int var2 = scan.nextInt();
+        try{
+            int var2 = scan.nextInt();
+        }
+        catch (Exception e) {
+            System.out.println("Du skrev fel, ange siffor");
+        }
     }
 }
