4966e855a81b8270ad331f0edb78c3e62768b27f
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2024-03-20 19:10:08 +0100

    Kopierat från del c

--- src/App.java
+++ src/App.java
@@ -2,14 +2,10 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Ange vad 10 ska delas med");
-        int var1 = 10;
-        try{
-            int var2 = scan.nextInt();
-        }
-        catch (Exception e) {
-            System.out.println("Du skrev fel, ange siffor");
-        }
+        int[] fält = {6,3,5,9};
+
+        for (int i = 0; i < 10; i++) {
+        System.out.println("element nr "+i+" har värdet "+fält[i]);
     }
+}
 }
